215b416ce786618ebddbbeb5bcd4b7937e77b708
timkenar <chepkenertimothy@gmail.com>
2025-01-10 17:07:24 +0300

    Changed the debug settings

--- witeithie_backend/settings.py
+++ witeithie_backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY ='django-insecure-h@5^@f+op0ti%*jm+d%dp)x$2e41av(*z!74t!*$tu53s59pae'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 
 # Allowed hosts (set dynamically from the environment)
