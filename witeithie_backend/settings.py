b35ec0ddce3919a4736a5e4581a6be80d09349b9
timkenar <chepkenertimothy@gmail.com>
2025-01-11 22:08:46 +0300

    Added a command for static files

--- witeithie_backend/settings.py
+++ witeithie_backend/settings.py
@@ -154,7 +154,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# The location where static files will be collected to:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
